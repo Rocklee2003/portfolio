36a602a266d59f8ddf9965a7ee52026ab8e931e9
Utkarsh Ahirrao <155551612+Rocklee2003@users.noreply.github.com>
2024-03-02 01:22:57 -0800

    main page js file

---
+++ portfolio1.js
@@ -0,0 +1,55 @@
+
+function openNewTab1() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://github.com/Rocklee2003/python-project/blob/main/tictactoeproject.py', '_blank');
+}
+function openNewTab2() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://github.com/Rocklee2003/python-project/blob/main/StonePaperScissor.py', '_blank');
+}
+function openNewTab3() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://github.com/Rocklee2003/Frontend-Designs./blob/main/demono.1.html', '_blank');
+}
+
+function linkedin() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://www.linkedin.com/in/utkarsh-ahirrao-794826259/', '_blank');
+}
+function instagram() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://www.instagram.com/ek_musafir_2003?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==', '_blank');
+}
+
+function openNewTab8() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://github.com/Rocklee2003', '_blank');}
+
+    function openNewTab9() {
+        // Replace 'https://example.com' with the URL you want to open
+        window.open('project1.html', '_blank');}
+
+        function wireshark() {
+            // Replace 'https://example.com' with the URL you want to open
+            window.open('https://drive.google.com/file/d/1gEf-XMrG9wfO4haWGLEk-22Uwd-YU53v/view?usp=drive_link', '_blank');
+        }function nmap() {
+            // Replace 'https://example.com' with the URL you want to open
+            window.open('https://drive.google.com/file/d/1gm6-qTiVu5Z7TaPuKNyGp3fJCsvrCX7G/view?usp=drive_link', '_blank');
+        }function Metasploit() {
+            // Replace 'https://example.com' with the URL you want to open
+            window.open('https://academy.hackthebox.com/achievement/1101055/39', '_blank');
+        }
+function python() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://drive.google.com/file/d/1jaUie4sVwpAd9o6tcy-BbwKx1AxI8Azg/view?usp=drive_link', '_blank');
+  }
+
+  function google() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://drive.google.com/file/d/1btmYcICS-1yoYc8hwJnSIdnxajyIDAVw/view?usp=drive_link', '_blank');
+  }  
+
+  function watch() {
+    // Replace 'https://example.com' with the URL you want to open
+    window.open('https://www.linkedin.com/posts/utkarsh-ahirrao-794826259_webdesign-webdevelopment-websitelaunch-activity-7121845449979813888-Z8jM?utm_source=share&utm_medium=member_desktop', '_blank');
+}
